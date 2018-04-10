f61a1b211c8aed632c8cadb7af78871aede7639e
Ahmed-Anwer <ahmed.anwer.ezzeldien@gmail.com>
2018-04-10 12:02:04 +0300

    Initail Commit1

--- app/src/main/java/com/ahmedelbossily/chatapp/LoginActivity.java
+++ app/src/main/java/com/ahmedelbossily/chatapp/LoginActivity.java
@@ -1,17 +1,27 @@
 package com.ahmedelbossily.chatapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
     private AutoCompleteTextView login_email;
     private EditText login_password;
 
@@ -35,6 +45,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         // TODO: Grab an instance of FirebaseAuth
+        auth = FirebaseAuth.getInstance();
     }
 
     // Executed when Sign in button pressed
@@ -50,8 +61,36 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void attemptLogin() {
+        String email = login_email.getText().toString();
+        String password = login_password.getText().toString();
+
+        if (email.equals("") || password.equals("")) return;
+        Toast.makeText(this, "Login in progress...", Toast.LENGTH_SHORT).show();
+
         // TODO: Use FirebaseAuth to sign in with email & password
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("ChatApp", "signInWithEmail() onComplete: " + task.isSuccessful());
+                if (!task.isSuccessful()) {
+                    Log.d("ChatApp", "Problem signing in: " + task.getException());
+                    showErrorDialog("There was a problem signing in");
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, MainChatActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     // TODO: Show error on screen with an alert dialog
+    private void showErrorDialog(String message) {
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 }
--- app/src/main/java/com/ahmedelbossily/chatapp/MainChatActivity.java
+++ app/src/main/java/com/ahmedelbossily/chatapp/MainChatActivity.java
@@ -1,10 +1,18 @@
 package com.ahmedelbossily.chatapp;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainChatActivity extends AppCompatActivity {
 
@@ -12,6 +20,9 @@ public class MainChatActivity extends AppCompatActivity {
     private ListView chat_list_view;
     private EditText messageInput;
     private ImageButton sendButton;
+    private ChatListAdapter adapter;
+
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,28 +30,62 @@ public class MainChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_chat);
 
         // TODO: Set up the display name and get the Firebase reference
+        setupDisplayName();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
         chat_list_view = findViewById(R.id.chat_list_view);
         messageInput = findViewById(R.id.messageInput);
         sendButton = findViewById(R.id.sendButton);
 
         // TODO: Send the message when the "enter" button is pressed
+        messageInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                sendMessage();
+                return true;
+            }
+        });
 
         // TODO: Add an OnClickListener to the sendButton to send a message
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
     }
 
     // TODO: Retrieve the display name from the Shared Preferences
+    private void setupDisplayName() {
+        SharedPreferences preferences = getSharedPreferences(RegisterActivity.CHAT_PREFS, MODE_PRIVATE);
+        displayName = preferences.getString(RegisterActivity.DISPLAY_NAME_KEY, null);
+        if (displayName == null)
+            displayName = "Anonymous";
+    }
 
     private void sendMessage() {
+        Log.d("ChatApp", "I send something");
         // TODO: Grab the text the user typed in and push the message to Firebase
+        String input = messageInput.getText().toString();
+        if (!input.equals("")) {
+            InstantMessage chat = new InstantMessage(input, displayName);
+            databaseReference.child("messages").push().setValue(chat);
+            messageInput.setText("");
+        }
     }
 
     // TODO: Override the onStart() lifecycle method. Setup the adapter here.
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter = new ChatListAdapter(this, databaseReference, displayName);
+        chat_list_view.setAdapter(adapter);
+    }
 
     @Override
     protected void onStop() {
         super.onStop();
         // TODO: Remove the Firebase event listener on the adapter.
+        adapter.cleanUp();
     }
 }
